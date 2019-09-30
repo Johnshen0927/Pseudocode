86ecff4f80e798323cf6fa1685541368e306065d
Johsnehn <ll122364919@gmail.com>
2019-09-30 22:50:38 +0800

    Critical Path

--- Graph/Top.c
+++ Graph/Top.c
@@ -122,3 +122,41 @@ Status ToplogicalSort(GraphAdjList GL)
         return OK;
     }
 }
+
+/* 求关键路径，GL为有向网，输出GL的各项关键活动 */
+void CriticalPath(GraphAdjList GL)
+{
+    EdgeNode *e;
+    int i, gettop, k, j;
+    int ete, lte;                                       /* 声明活动最早 开工 时间，最迟 开工 时间变量 */
+    ToplogicalSort(GL);                                 /* 求拓扑序列，计算数组etv和stack2的值 */
+    ltv = (int *)malloc(GL->numVertexes * sizeof(int)); /* 事件最晚发生时间 */
+    for (i = 0; i < GL->numVertexes; i++)
+    {
+        ltv[i] = etv[GL->numVertexes - 1]; /* 初始化ltv */
+    }
+    while (top2 != 0)
+    {
+        gettop = stack2[top2--]; /* 将拓扑序列出栈，后进先出 */
+        for (e = GL->adjList[gettop].firstedge; e; e = e->next)
+        { /* 求各顶点事件最迟发生时间ltv的值 */
+            k = e->adjvex;
+            if (ltv[k] - e->weight < ltv[gettop])
+            {
+                /* 求各顶点事件最晚发生时间ltv */
+                ltv[gettop] = ltv[k] - e->weight;
+            }
+        }
+    }
+    for (j = 0; j < GL->numVertexes; j++) /* 求ete，lte和关键活动 */
+    {
+        for (e = GL->adjList[j].firstedge; e; e = e->next)
+        {
+            k = e->adjvex;
+            ete = etv[j];             /* 活动最早发生时间 */
+            lte = ltv[k] - e->weight; /* 活动最迟发生时间 */
+            if (ete == lte)           /* 两者相等，即在关键路径上 */
+                printf("<v%d,v%d>length:%d,", GL->adjList[j].data, GL->adjList[k].data, e->weight);
+        }
+    }
+}
