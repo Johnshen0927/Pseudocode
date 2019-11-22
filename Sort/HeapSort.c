bb03453179abb81ef4629458e916f954b2f56114
Johsnehn <ll122364919@gmail.com>
2019-11-22 17:39:33 +0800

    cy

--- Sort/HeapSort.c
+++ Sort/HeapSort.c
@@ -1,9 +1,24 @@
 #include "Basic.c"
 
-void HeapAdjust(Sqlist *L, int s, int m)
+void HeapAdjust(Sqlist *L, int s, int m) /* 构建从s....m的大顶堆 */
 {
+    int temp, j;
+    temp = L->r[s];
+    for (j = 2*s; j <= m; j *= 2)
+    {
+        
+    }
 }
 void HeapSort(Sqlist *L)
 {
     int i;
+    for (i = L->length / 2; i > 0; i--) /* 循环数组来将其构建成大顶堆 */
+    {
+        HeapAdjust(L, i, L->length);
+    }
+    for (i = L->length; i > 1; i--)
+    {
+        swap(L, 1, i);           /* 交换堆顶和堆末的元素 */
+        HeapAdjust(L, 1, i - 1); /* 重新构建长度-1的大顶堆 */
+    }
 }
